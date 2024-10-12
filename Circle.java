e42680268b12c2a41069dacbfb1a3b32178b9375
Hussain Harianawala <hussain.h@somaiya.edu>
2024-10-12 08:52:03 +0000

    First Commit

---
+++ Circle.java
@@ -0,0 +1,20 @@
+public class Circle {
+    public static void main(String[] args) {
+       
+        double r = 5.0; 
+
+        // Declare variable for area and pi
+        double a, pi;
+
+        // Assign value to pi
+        pi = 22.0 / 7;
+
+        // Calculate the area
+        a = pi * r * r;
+
+        // Display results
+        System.out.println("Radius of the circle = " + r);
+        System.out.println("Area of the circle = " + a);
+    }
+}
+
